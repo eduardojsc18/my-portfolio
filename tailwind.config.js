981c57f9018d546894a7c6f64dd8eb053cc4644e
Eduardo José <81324243+eduardojsc18@users.noreply.github.com>
2021-11-13 21:19:29 -0300

    Project started

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    enabled: false,
+    content: ['./resources/view/*.html'],
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
