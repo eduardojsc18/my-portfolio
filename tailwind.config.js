4df6a77987a8653867b442de42db51f159d95707
Eduardo José <81324243+eduardojsc18@users.noreply.github.com>
2021-11-17 18:18:04 -0300

    - Ajustes de FrontEnd
    - Realizado ajustes na apresentação da pagina
    - Ajustes no menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,47 +42,56 @@ module.exports = {
 
             borderRadius: {
                 'shapes-perfil': '39% 53% 22% 28% / 28% 45% 46% 55%',
-            }
+            },
 
-        },
+            animation: {
+                'gradient-xy': 'gradient-xy 10s ease infinite',
+                'shape-perfil': 'shape-perfil 25s ease-in-out alternate infinite ',
+                'shape-background-perfil': 'shape-background-perfil 20s ease-in-out alternate infinite',
+                'wiggle': 'wiggle 1s ease-in-out infinite',
+            },
 
-        animation: {
+            keyframes: {
 
-            'gradient-xy': 'gradient-xy 10s ease infinite',
-            'shape-perfil': 'shape-perfil 25s ease infinite',
-            'shape-background-perfil': 'shape-background-perfil 25s ease infinite'
-        },
-
-        keyframes: {
-
-            'gradient-xy': {
-                '0%, 100%': {
-                    'background-size': '400% 400%',
-                    'background-position': 'left center'
+                'gradient-xy': {
+                    '0%, 100%': {
+                        'background-size': '400% 400%',
+                        'background-position': 'left center'
+                    },
+                    '50%': {
+                        'background-size':'100% 100%',
+                        'background-position': 'right center'
+                    }
                 },
-                '50%': {
-                    'background-size':'100% 100%',
-                    'background-position': 'right center'
+
+                'shape-perfil': {
+                    '0%': { borderRadius: '39% 53% 22% 28% / 28% 45% 46% 55%' },
+                    '25%': { borderRadius: '65% 35% 41% 59% / 73% 37% 63% 27%' },
+                    '50%': { borderRadius: '52% 48% 51% 49% / 29% 46% 54% 71% ' },
+                    '75%': { borderRadius: '45% 55% 13% 87% / 39% 75% 25% 61%' },
+                    '100%': { borderRadius: '39% 53% 22% 28% / 28% 45% 46% 55%' },
+                },
+
+                'shape-background-perfil': {
+                    '0%': { borderRadius: '45% 55% 13% 87% / 83% 75% 25% 17%' },
+                    '25%': { borderRadius: '30% 70% 70% 30% / 30% 27% 73% 70%' },
+                    '50%': { borderRadius: '57% 43% 70% 30% / 67% 27% 73% 33%' },
+                    '75%': { borderRadius: '41% 59% 51% 49% / 43% 51% 49% 57%' },
+                    '100%': { borderRadius: '45% 55% 13% 87% / 83% 75% 25% 17%' },
+                },
+
+                'wiggle': {
+                    '0%, 100%': {
+                        transform: 'rotate(-2deg)'
+                    },
+                    '50%': {
+                        transform: 'rotate(2deg)'
+                    },
                 }
-            },
 
-            'shape-perfil': {
-                '0%': { borderRadius: '39% 53% 22% 28% / 28% 45% 46% 55%' },
-                '25%': { borderRadius: '65% 35% 41% 59% / 73% 37% 63% 27%' },
-                '50%': { borderRadius: '52% 48% 51% 49% / 29% 46% 54% 71% ' },
-                '75%': { borderRadius: '45% 55% 13% 87% / 39% 75% 25% 61%' },
-                '100%': { borderRadius: '39% 53% 22% 28% / 28% 45% 46% 55%' },
-            },
-
-            'shape-background-perfil': {
-                '0%': { borderRadius: '45% 55% 13% 87% / 83% 75% 25% 17%' },
-                '25%': { borderRadius: '30% 70% 70% 30% / 30% 27% 73% 70%' },
-                '50%': { borderRadius: '57% 43% 70% 30% / 67% 27% 73% 33%' },
-                '75%': { borderRadius: '41% 59% 51% 49% / 43% 51% 49% 57%' },
-                '100%': { borderRadius: '45% 55% 13% 87% / 83% 75% 25% 17%' },
             }
 
-        }
+        },
 
     },
     variants: {
@@ -90,7 +99,9 @@ module.exports = {
 
             scale: ['group-hover'],
             display: ['group-hover'],
-            animation: ['group-hover'],
+            animation: ['group-hover', 'hover'],
+            translate: ['group-hover'],
+            overflow: ['hover']
 
         },
     },
